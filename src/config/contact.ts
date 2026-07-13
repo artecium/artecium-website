9d8425f4e24fed221cfe5f08057e0b4b5ee7e75f
artecium <rodrigo.martins2004@icloud.com>
2026-07-13 23:05:08 +0100

    Nwe Phone Number

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -1,4 +1,4 @@
-export const COMPANY_PHONE = "351964291946";
+export const COMPANY_PHONE = "351934549190";
 export const COMPANY_EMAIL = "hello@artecium.com";
 export const COMPANY_NAME = "Artecium";
 export const COMPANY_WEBSITE = "https://artecium.com";
